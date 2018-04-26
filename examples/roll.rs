47796f1979b75f9b242777609cdc4ca24530ea8d
Mattias Cibien <mattias@mattiascibien.net>
2018-04-26 11:53:58 +0200

    Init roll example

--- examples/roll.rs
+++ examples/roll.rs
@@ -1,7 +1,21 @@
 extern crate dicenotation;
 extern crate clap;
 
-fn main()
-{
-    
+use clap::{Arg, App};
+
+fn main() {
+    let matches = App::new("Dice Roller")
+                .version("1.0")
+                .author("Mattias Cibien <mattias@mattiascibien.net>")
+                .about("Rolls dice using Standard Dice Notation")
+                .arg(Arg::with_name("dicestring")
+                    .required(true)
+                    .index(1)
+                    .takes_value(true))
+                .get_matches();
+
+     let dicestring = matches.value_of("dicestring").unwrap();
+
+     println!("Rolling: {}", dicestring);
+     // TODO: roll the dice using the library
 }
